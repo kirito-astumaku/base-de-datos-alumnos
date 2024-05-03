f6da97b0c140221993679a46f1708e53c2cd76e4
Manuel moreno <mfmoreno092000@gmail.com>
2024-05-02 19:39:45 -0600

    listo

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+function nuevo_estudiante(){
+    document.getElementById('registro_1').style.display = 'block'
+    document.getElementById('inicioDesecion_1').style.display = 'none'
+}
+
+function registro(){
+    document.getElementById('inicioDesecion_1').style.display = 'block'
+    document.getElementById('registro_1').style.display = 'none'
+}
