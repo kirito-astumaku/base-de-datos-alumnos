b36aca8ff2d8356d1b72423f0fe8b926c7034ea2
Manuel moreno <mfmoreno092000@gmail.com>
2024-05-05 14:57:13 -0600

    nueva lista

--- base_De_datos_AL.js
+++ base_De_datos_AL.js
@@ -1,6 +1,198 @@
 
 
+class Node{
+    constructor(data){
+        this.data = data
+        this.next = null
+    }
+}
 
+class Linkedlist{
+    constructor(head = null){
+         this.head = head
+    }
+
+    insert(data){
+       let nudo = new Node(data)
+
+       if(this.head == null){
+          this.head = nudo
+       }else{
+        let current = this.head
+        while(current.next != null){
+            current = current.next
+            
+        }
+
+         current.next = nudo
+        
+       }
+    }
+
+    delete(data){
+        let current = this.head
+        let prev = this.head
+        if(this.head != null){
+           if(this.head.data == data){
+               current = current.next
+               this.head = current
+           }else{
+            while(current.data !== data && current.next != null){
+               prev = current
+               current = current.next
+
+            }
+
+            if(current.data == data){
+                prev.next = current.next
+            }else{
+                console.log('no hay ningun dato')
+            }
+        }
+        }else{
+            console.log('la lista esta vacia ')
+        }
+    }
+
+
+    print(){
+        let linkedliststring = ''
+        let current = this.head
+        while(true){
+           if(current == null){
+            break
+
+            linkedliststring += `${current.data} ->`
+            current = current.next
+           }
+        }
+
+        return linkedliststring = linkedliststring + 'null'
+    }
+
+
+     searchIFExist(data){
+        let current = this.head
+        while(current.data !== data && current.next !== null){
+             current = current.next
+        }
+
+        return current !== null
+    } 
+}
+
+
+
+let linkedlist = new Linkedlist()
+
+linkedlist.insert(1)
+linkedlist.insert(2)
+linkedlist.insert(3)
+linkedlist.insert(4)
+
+console.log(linkedlist)
+
+linkedlist.delete(2)
+
+console.log(linkedlist)
+
+console.log(linkedlist.print())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 let nombre_3 = document.getElementById('nombre_3')
 let seccion =  document.getElementById('seccion')
 let asisgnatura_3 = document.getElementById('asisgnatura_3')
@@ -14,12 +206,12 @@ class aLUMnosnuevos{
         this.agisnatura = agisnatura
     }
 
-    nomBre_(){
-        nombre_3.innerHTML = this.nombre
+    nombre_4(hola){
+        nombre_3.innerHTML = hola
     }
 }
 
- let save = [] 
+ let save = ['','','',''] 
 
 function btn_Registro(){
     let Nombre = document.getElementById('nombre_1').value
@@ -28,17 +220,11 @@ function btn_Registro(){
     let Agsnatura = document.getElementById('asisgnatura_V2').value
     let alumnos = new aLUMnosnuevos(Nombre,Apellido,edad,Agsnatura)
     console.log(alumnos)
-    save.push()
-    console.log(save[0])
-    nomBre_()
+    save = this.nombre
     document.getElementById('registro_12').style.display = 'block'
     document.getElementById('registro_1').style.display = 'none'
-
+    document.getElementById('btno').style.display = 'none'
+    nombre_3.innerHTML = this.nombre
+     
 }
-
-
-
-
-
-
-
+ */
